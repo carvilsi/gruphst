c07c60e87762d3b5d949021b5cdfcbf7cc21b593
carvilsi <carvilsi.mail.list@gmail.com>
2024-06-23 16:36:25 +0200

    before some refactoring to slipt on different files

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,22 @@ use std::io::Read;
 use std::error::Error;
 use serde::{Deserialize, Serialize};
 use std::mem;
+use uuid::Uuid;
+use std::fs::OpenOptions;
+
+static DATA_FILE: &str = "gruphst.bin";
 
 #[derive(Debug, Clone)]
 #[derive(Serialize, Deserialize)]
 struct Node {
-    id: i32,
+    id: String,
     name: String,
 }
 
 #[derive(Debug, Clone)]
 #[derive(Serialize, Deserialize)]
 struct Graph {
-    id: i32,
+    id: String,
     relation: String,
     from: Node,
     to: Node,
@@ -28,57 +32,73 @@ struct Header {
 }
 
 trait Gruphst {
-    fn new(name: String) -> Self;
     fn name(&self) -> String;
 }
 
 impl Gruphst for Node {
-    fn new(name: String) -> Node {
-        Node { name, id: 1 }
-    }
     fn name(&self)  -> String {
         self.name.clone()
     }
 }
 
-fn main()-> Result<(), Box<dyn Error>> {    
-    let file_name = "gruphst.bin";
-    let mut file = File::create(&file_name)?;
-    
-    let node1 = Node {
-        id: 0,
-        name: String::from("this is an origin node"),
-    };
-    
-    let n = node1.clone();
- 
-    let node2 = Node {
-        name: String::from("this is target node"),
-        id: 1
-    };
+impl Node {
+    fn new(name: String) -> Node {
+        Node {
+            name,
+            id: Uuid::new_v4().to_string()
+        }
+    }
+}
 
-    let graph = Graph {
-        relation: String::from("related to moar tingys"),
-        id: 2,
-        from: node1,
-        to: node2,
-    };
+impl Graph {
+    fn new(name: String, from: Node, to: Node) -> Graph {
+        Graph { 
+            relation: name,
+            id: Uuid::new_v4().to_string(),
+            from,
+            to,
+        }
+    }
+    fn persists(&self) -> Result<(), Box<dyn Error>> {
+        let mut file = OpenOptions::new().create(true).append(true).open(&DATA_FILE)?;
+        let bytes = bincode::serialize(self)?;
+        let written = file.write(&bytes)?;
+        println!("{} bytes has been written", written);
+        println!("The bytes: {:?}", bytes);
+        println!("The size of the bytes: {}", bytes.len());            
+        Ok(())
+    }
+
+}
+
+impl Gruphst for Graph {
+    fn name(&self) -> String {
+        self.relation.clone()
+    }
+}
+
+fn main()-> Result<(), Box<dyn Error>> {    
+   
+    let node1 = Node::new(String::from("Node 1")); 
+    let node2 = Node::new(String::from("Node 2")); 
+    let n1 = node1.clone();
+    let n2 = node2.clone();
+    let graph = Graph::new(String::from("related to"), node1, node2);
     let g = graph.clone();
     
     println!("graph: {:#?}", graph);
-    println!("mem of node1: {}", mem::size_of_val(&n)); 
     println!("mem of graph: {}", mem::size_of_val(&g)); 
 
-    let bytes = bincode::serialize(&graph)?;
-    let written = file.write(&bytes)?;
-    println!("{} bytes has been written", written);
-    println!("The bytes: {:?}", bytes);
-    println!("The size of the bytes: {}", bytes.len());
+    graph.persists()?;
+    
+    println!("The name of node1: {}", n1.name());
+    println!("The name of node2: {}", n2.name());
+    println!("The name of graph: {}", graph.name());
 
     println!("----------");
 
-    let mut read_file = File::open(&file_name)?;
-    let mut buffer = [0; 99];
+    let mut read_file = File::open(&DATA_FILE)?;
+    let mut buffer = [0; 178];
     let _ = read_file.read(&mut buffer[..])?;
     println!("The readed bytes: {:?}", buffer);
     let readed_graph: Graph = bincode::deserialize(&buffer)?;
