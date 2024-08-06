2ad51dc764d1e55b93b6b3cff31dae3093a7bc9e
carvilsi <carvilsi.mail.list@gmail.com>
2024-08-06 15:22:18 +0200

    updating example

--- examples/rock-paper-scissors-lizard-spock/src/main.rs
+++ examples/rock-paper-scissors-lizard-spock/src/main.rs
@@ -1,6 +1,6 @@
-use gruphst::{edge::Edge, graph::Graph, graphs::Graphs, *};
-use std::collections::HashMap;
+use gruphst::{edge::Edge, graphs::Graphs, vertex::Vertex};
 use rand::Rng;
+use std::collections::HashMap;
 
 fn create_game_rules() -> Graphs {
     // Create the sets of edges and add some attributes
@@ -23,25 +23,25 @@ fn create_game_rules() -> Graphs {
     let mut graphs = Graphs::init("game-rules");
 
     // Now we create the first relation between two edges
-    // and we'll add to the vault's default graph 
-    let graph = Graph::create(&rock, "crushes", &lizard);
+    // and we'll add to the vault's default graph
+    let graph = Vertex::create(&rock, "crushes", &lizard);
     graphs.add_graph(&graph, None);
 
     // More relations will be added
-    graphs.add_graph(&Graph::create(&rock, "crushes", &scissors), None);
-    graphs.add_graph(&Graph::create(&lizard, "poisons", &spock), None);
-    graphs.add_graph(&Graph::create(&lizard, "eats", &paper), None);
-    graphs.add_graph(&Graph::create(&spock, "smashes", &scissors), None);
-    graphs.add_graph(&Graph::create(&spock, "vaporizes", &rock), None);
-    graphs.add_graph(&Graph::create(&scissors, "cuts", &paper), None);
-    graphs.add_graph(&Graph::create(&scissors, "decapites", &lizard), None);
-    graphs.add_graph(&Graph::create(&paper, "covers", &rock), None);
-    graphs.add_graph(&Graph::create(&paper, "disproves", &spock), None);
+    graphs.add_graph(&Vertex::create(&rock, "crushes", &scissors), None);
+    graphs.add_graph(&Vertex::create(&lizard, "poisons", &spock), None);
+    graphs.add_graph(&Vertex::create(&lizard, "eats", &paper), None);
+    graphs.add_graph(&Vertex::create(&spock, "smashes", &scissors), None);
+    graphs.add_graph(&Vertex::create(&spock, "vaporizes", &rock), None);
+    graphs.add_graph(&Vertex::create(&scissors, "cuts", &paper), None);
+    graphs.add_graph(&Vertex::create(&scissors, "decapites", &lizard), None);
+    graphs.add_graph(&Vertex::create(&paper, "covers", &rock), None);
+    graphs.add_graph(&Vertex::create(&paper, "disproves", &spock), None);
 
     graphs
 }
 
-fn who_wins(wins: HashMap<String, Vec<edge>>, a_game: &edge, b_game: &edge) -> Option<String> {
+fn who_wins(wins: HashMap<String, Vec<Edge>>, a_game: &Edge, b_game: &Edge) -> Option<String> {
     for (action, targets) in wins.iter() {
         for target in targets {
             if target.get_id() == b_game.get_id() {
@@ -53,15 +53,15 @@ fn who_wins(wins: HashMap<String, Vec<edge>>, a_game: &edge, b_game: &edge) -> O
     None
 }
 
-fn resolve_game(player_one_game: edge, player_two_game: edge, rules: Graphs) -> String {
-    let one = format!("player one plays: {}", player_one_game.get_label());
+fn resolve_game(player_one_game: Edge, player_two_game: Edge, rules: Graphs) -> String {
+    let one = format!("Player one plays: {}", player_one_game.get_label());
     let player_one_wins_to = player_one_game
         .get_relations_out_on_graph(rules.get_graphs(Some("game-rules")).unwrap())
         .unwrap();
     let player_two_wins_to = player_two_game
         .get_relations_out_on_graph(rules.get_graphs(Some("game-rules")).unwrap())
         .unwrap();
-    let two = format!("player two plays: {}", player_two_game.get_label());
+    let two = format!("Player two plays: {}", player_two_game.get_label());
 
     let mut game_result = String::from("");
     if player_one_game.get_label() == player_two_game.get_label() {
@@ -77,25 +77,31 @@ fn resolve_game(player_one_game: edge, player_two_game: edge, rules: Graphs) -> 
     if res.is_some() {
         game_result = format!("{}\nPlayer two wins!", res.unwrap());
     }
-    
+
     format!("{}\n{}\n{}", one, two, game_result)
 }
 
 fn main() {
     let rules = create_game_rules();
 
-    // maybe now you want to persists the game rules 
+    // maybe now you want to persists the game rules
     // to use it other day ;)
-    let _ = rules.persists(); 
+    let _ = rules.persists();
 
     // get the characters
     let characters = rules.get_uniq_edges(None).unwrap();
 
     // Lets play a bit
-    let mut rand_number: usize = rand::thread_rng().gen_range(0..characters.len()).try_into().unwrap();
-    let player_one_game: &edge = &characters[rand_number];
-    rand_number = rand::thread_rng().gen_range(0..characters.len()).try_into().unwrap();
-    let player_two_game: &edge = &characters[rand_number];
+    let mut rand_number: usize = rand::thread_rng()
+        .gen_range(0..characters.len())
+        .try_into()
+        .unwrap();
+    let player_one_game: &Edge = &characters[rand_number];
+    rand_number = rand::thread_rng()
+        .gen_range(0..characters.len())
+        .try_into()
+        .unwrap();
+    let player_two_game: &Edge = &characters[rand_number];
     let result = resolve_game(player_one_game.clone(), player_two_game.clone(), rules);
     println!("{}", result);
 }
