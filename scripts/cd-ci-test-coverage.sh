a6803346745b3b468df26bfa111b16f5d69cf1d8
carvilsi <carvilsi.mail.list@gmail.com>
2024-08-13 15:40:43 +0200

    just one line left to reach 100%...

--- scripts/cd-ci-test-coverage.sh
+++ scripts/cd-ci-test-coverage.sh
@@ -3,7 +3,5 @@
 # requires tarpaulin
 # install it with: $ cargo install cargo-tarpaulin
 
-mv .env .env-tmp
-cp .env-test .env
-cargo +nightly tarpaulin --ignore-tests --verbose --all-features --workspace --timeout 120 --out xml
-mv .env-tmp .env 
+mv .env-test .env
+cargo tarpaulin --ignore-tests --timeout 120 --out xml
