dac2f157037233f2ef3b0094ec7c65461c4b64f1
carvilsi <carvilsi.mail.list@gmail.com>
2024-08-28 17:26:02 +0200

    trying to fix tests for persistence, something related with env

--- tests/persistence_test.rs
+++ tests/persistence_test.rs
@@ -58,7 +58,8 @@ fn should_persists_on_custom_path() {
         Ok(grphs) => {
             assertion_persisted_graphs(grphs, name, edge1, edge2);
         }
-        Err(_) => {
+        Err(err) => {
+            println!("{err}");
             panic!()
         },
     }
