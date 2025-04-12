c322f041a8a19b34317dccce755d01032bd192a6
carvilsi <carvilsi.mail.list@gmail.com>
2025-04-12 16:02:47 +0200

    target of 100% test code coverage

--- src/edge/queries/vertices.rs
+++ src/edge/queries/vertices.rs
@@ -26,15 +26,13 @@ impl Edge {
     }
 
     /// Checks if "from" or "to" vertex has a string attribute and equal for value
+    #[rustfmt::skip]
     pub fn has_vertex_with_attr_str_value_equals_to<T>(&self, attr_k: &str, attr_v: T) -> bool
     where
         T: std::fmt::Display + std::clone::Clone,
     {
-        #[cfg_attr(tarpaulin, ignore)]
         self.get_from_vertex().has_attr_str_equals_to(attr_k, attr_v.clone())
-            || self
-                .get_to_vertex()
-                .has_attr_str_equals_to(attr_k, attr_v.clone())
+            || self.get_to_vertex().has_attr_str_equals_to(attr_k, attr_v.clone())
     }
 
     /// Checks if "from" or "to" vertices has a Vec<u8> attribute key
