4f47fba1d3b3d95e2b6433ab5eac0f6b9acd5b9f
carvilsi <carvilsi.mail.list@gmail.com>
2025-04-12 17:02:36 +0200

    more fmt skip...

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -22,10 +22,8 @@ pub(crate) fn graphs_memory_watcher(graphs: &Graphs) {
         }
         mem_prss if mem_prss >= 99_f32 => {
             error!("memory usage critical: {:.2}", mem_prss);
-            error!(
-                "auto persisting current graphs: {}, then panicking",
-                graphs.get_label()
-            );
+            #[rustfmt::skip]
+            error!("auto persisting current graphs: {}, then panicking", graphs.get_label());
             let _ = graphs.save(None);
             panic!("memory usage critical, auto-persisted current graphs");
         }
